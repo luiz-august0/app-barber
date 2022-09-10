53f466442e45a968355de9420d42c9d68b16ff47
luiz-august0 <luizaugustom030502@gmail.com>
2022-09-09 21:31:17 -0300

    Implementando API e banco de dados

---
+++ api/routes/UsuarioController.js
@@ -0,0 +1,173 @@
+import { createPasswordHash } from '../services/auth';
+
+const mysql = require('../config/mysql').pool;
+
+class UsuarioController {
+    async index(req, res) {
+        try {
+            mysql.getConnection((error, conn) => {
+                conn.query(
+                    `SELECT * FROM usuario`,
+                    (error, result, fields) => {
+                        if (error) { return res.status(500).send({ error: error }) }
+                        return res.status(201).json(result);
+                    }
+                )
+                conn.release();
+            })
+        } catch(err) {
+            console.error(err);
+            return res.status(500).json({ error: "Internal server error." })
+        }
+    }
+
+    async show (req, res) {
+        try {
+            const { id } = req.params;
+
+            mysql.getConnection((error, conn) => {
+                conn.query(
+                    `SELECT * from usuario WHERE Usr_Codigo = ${id}`,
+                    (error, result, fields) => {
+                        if (error) { return res.status(500).send({ error: error }) }
+                        if (!result) {
+                            return res.status(404).json();
+                        }
+                        return res.status(201).json(result);
+                    }
+                )
+                conn.release();
+            });
+        } catch (err) {
+            console.error(err);
+            return res.status(500).json({ error: "Internal server error." });
+        }
+    }
+
+    async create(req, res) {
+        try {
+            const { usuario, senha, setor } = req.body;
+
+            const encryptedPassword = await createPasswordHash(senha);
+            
+            mysql.getConnection((error, conn) => {
+                conn.query(
+                    `SELECT * FROM usuario WHERE Usr_Login = "${usuario}"`,
+                    (error, result, fields) => {
+                        if (error) { return res.status(500).send({ error: error }) }
+                    
+                        if (JSON.stringify(result) != '[]') {
+                            return res.status(401).json('Usuário já cadastrado');
+                        } else {
+                            conn.query(
+                                `INSERT INTO usuario (Usr_Login, Usr_Senha, Str_Codigo) VALUES ("${usuario}","${encryptedPassword}","${setor}")`,
+                                (error, result, fields) => {
+                                    if (error) { return res.status(500).send({ error: error }) }
+                                    return res.status(201).json(result);
+                                }
+                            )
+                        }
+                    }
+                )
+                conn.release();
+            });
+        } catch(err) {
+            console.error(err);
+            return res.status(500).json({ error: "Internal server error." });
+        }
+    }
+
+    async update(req, res) {
+        try {
+            const { id } = req.params;
+            const { usuario, senha, setor } = req.body;
+            const encryptedPassword = await createPasswordHash(senha);
+
+            mysql.getConnection((error, conn) => {
+                conn.query(
+                    `SELECT * FROM usuario WHERE Usr_Codigo = "${id}"`,
+                    (error, result, fields) => {
+                        if (error) { return res.status(500).send({ error: error }) }
+
+                        if (JSON.stringify(result) === '[]') {
+                            return res.status(404).json();
+                        }
+                        else {
+                            conn.query(
+                                `UPDATE usuario SET Usr_Login = "${usuario}", Usr_Senha = "${encryptedPassword}", Str_Codigo = "${setor}" WHERE Usr_Codigo = "${id}"`,
+                            (error, result, fields) => {
+                                if (error) { return res.status(500).send({ error: error }) }
+                                return res.status(201).json(result);
+                            }
+                            )
+                        }
+                    }
+                )
+                conn.release();
+            });
+            
+        } catch (err) {
+            console.error(err);
+            return res.status(500).json({ error: "Internal server error." });
+        }
+    }
+
+    async destroy(req, res) {
+        try {
+            const { id } = req.params;
+
+            if (id === '1') {
+                return res.status(404).json({ error: 'Usuário de administrador não pode ser deletado' });
+            }
+
+            mysql.getConnection((error, conn) => {
+                conn.query(
+                    `SELECT * FROM usuario WHERE Usr_Codigo = "${id}"`,
+                    (error, result, fields) => {
+                        if (error) { return res.status(500).send({ error: error }) }
+
+                        if (JSON.stringify(result) === '[]') {
+                            return res.status(404).json();
+                        }
+                        
+                        conn.query(
+                            `DELETE FROM usuario WHERE Usr_Codigo = "${id}"`,
+                        (error, result, fields) => {
+                            if (error) { return res.status(500).send({ error: error }) }
+                            return res.status(201).json(result);
+                        }
+                        )
+                       
+                    }
+                )
+                conn.release();
+            });
+
+        } catch (err) {
+            console.error(err);
+            return res.status(500).json({ error: "Internal server error." });
+        }
+    }
+
+    async indexSetor(req, res) {
+        try {
+            mysql.getConnection((error, conn) => {
+                conn.query(
+                    `SELECT * FROM setor`,
+                    (error, result, fields) => {
+                        if (error) { return res.status(500).send({ error: error }) }
+                        return res.status(201).json(result);
+                    }
+                )
+                conn.release();
+            })
+        } catch(err) {
+            console.error(err);
+            return res.status(500).json({ error: "Internal server error." })
+        }
+    }
+
+
+}
+
+export default new UsuarioController();
---
+++ mysql/create.sql
@@ -0,0 +1,61 @@
+CREATE TABLE usuario(
+    Usr_Codigo INT PRIMARY KEY AUTO_INCREMENT,
+    Usr_Email VARCHAR(100) NOT NULL,
+    Usr_Nome VARCHAR(80) NOT NULL,
+    Usr_Senha VARCHAR(50) NOT NULL,
+    Usr_Contato VARCHAR(20),
+    Usr_CPF VARCHAR(11),
+    Usr_Tipo CHAR(1) NOT NULL
+);
+
+CREATE TABLE barbearia(
+    Barb_Codigo INT PRIMARY KEY AUTO_INCREMENT,
+    Barb_Nome VARCHAR(255) NOT NULL,
+    Barb_RazaoSocial VARCHAR(255) NOT NULL,
+    Barb_CNPJ VARCHAR(14) NOT NULL,
+    Barb_InscEst VARCHAR(9) NOT NULL
+);
+
+CREATE TABLE barbearia_endereco(
+    BarbEnd_Codigo INT PRIMARY KEY AUTO_INCREMENT,
+    Barb_Codigo INT NOT NULL,
+    BarbEnd_Cidade VARCHAR(80) NOT NULL,
+    BarbEnd_CEP VARCHAR(8) NOT NULL,
+    BarbEnd_Rua VARCHAR(80) NOT NULL,
+    BarbEnd_Numero INT NOT NULL,
+    BarbEnd_Bairro VARCHAR(80) NOT NULL,
+    BarbEnd_Complemento VARCHAR(50),
+    BarbEnd_Contato1 VARCHAR(20) NOT NULL,
+    BarbEnd_Contato2 VARCHAR(20),
+    BarbEnd_Contato3 VARCHAR(20)
+);
+
+CREATE TABLE barbearia_proprietarios(
+    Barb_Codigo INT NOT NULL,
+    Usr_Codigo INT NOT NULL
+);
+
+CREATE TABLE servico_categorias(
+    ServCat_Codigo INT PRIMARY KEY AUTO_INCREMENT,
+    Barb_Codigo INT NOT NULL,
+    Serv_Cat_Nome VARCHAR(55) NOT NULL
+);
+
+CREATE TABLE servico(
+    Serv_Codigo INT PRIMARY KEY AUTO_INCREMENT,
+    Serv_Nome VARCHAR(55) NOT NULL,
+    ServCat_Codigo INT NOT NULL,
+    Serv_Valor FLOAT NOT NULL
+);
+
+ALTER TABLE barbearia_endereco ADD CONSTRAINT fk_endereco_barbeiro
+FOREIGN KEY(Barb_Codigo) REFERENCES barbearia(Barb_Codigo);
+
+ALTER TABLE barbearia_proprietarios ADD CONSTRAINT pk_barbearia_proprietario
+PRIMARY KEY(Barb_Codigo, Usr_Codigo);
+
+ALTER TABLE servico_categorias ADD CONSTRAINT fk_categoria_barbearia
+FOREIGN KEY(Barb_Codigo) REFERENCES barbearia(Barb_Codigo);
+
+ALTER TABLE servico ADD CONSTRAINT fk_servico_categoria
+FOREIGN KEY(ServCat_Codigo) REFERENCES servico_categorias(ServCat_Codigo);
