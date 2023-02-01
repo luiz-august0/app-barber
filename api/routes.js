327aa8319309feb70b27fdd8d64fbbf7f6bf21c9
luiz-august0 <66564172+luiz-august0@users.noreply.github.com>
2023-01-31 21:46:49 -0300

    Adicionando a api do app novamente

---
+++ api/routes.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import SessionController from "./routes/SessionController";
+import auth from "./middlewares/auth";
+import UsuarioController from "./routes/UsuarioController";
+
+const routes = new Router();
+
+routes.post('/usuario', UsuarioController.create);
+routes.post('/usuarioVerify', UsuarioController.verify);
+routes.put('/sessions', SessionController.create);
+routes.use(auth);
+
+//Rotas usuário
+routes.get('/usuario', UsuarioController.index);
+routes.get('/usuario/:id', UsuarioController.show);
+routes.put('/usuario/:id', UsuarioController.update);
+routes.delete('/usuario/:id', UsuarioController.destroy);
+
+export default routes;
