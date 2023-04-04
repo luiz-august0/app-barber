61f400fff69472e1a9e53ec9deb72baf28d75cfc
luiz-august0 <luizaugustom030502@gmail.com>
2023-04-04 16:02:39 -0300

    Criada estrutura para os agendamentos e avaliações

--- sql/create.sql
+++ sql/create.sql
@@ -46,18 +46,66 @@ CREATE TABLE servico_categorias(
 
 CREATE TABLE servico(
     Serv_Codigo INT PRIMARY KEY AUTO_INCREMENT,
-    Serv_Nome VARCHAR(55) NOT NULL,
+    Serv_Nome VARCHAR(120) NOT NULL,
     ServCat_Codigo INT NOT NULL,
-    Serv_Valor FLOAT NOT NULL
+    Serv_Valor FLOAT NOT NULL,
+    Serv_Duracao TIME NOT NULL
 );
 
-ALTER TABLE barbearia_contatos ADD CONSTRAINT fk_contatos_barbearia
+CREATE TABLE servico_imagens(
+    Serv_Codigo INT NOT NULL,
+    Img_Url VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE barbearia_horarios(
+    Barb_Codigo INT NOT NULL,
+    BarbH_Dia CHAR(3) NOT NULL,
+    BarbH_HoraInicio TIME NOT NULL,
+    BarbH_HoraFim TIME NOT NULL
+);
+
+CREATE TABLE horarios (
+    Id INT AUTO_INCREMENT,
+    Horario TIME NOT NULL,
+    PRIMARY KEY (Id)
+);
+
+CREATE TABLE agendamento(
+    Barb_Codigo INT NOT NULL,
+    Agdm_Barbeiro INT NOT NULL,
+    Usr_Codigo INT NOT NULL,
+    Serv_Codigo INT NOT NULL,
+    Agdm_HoraInicio TIME NOT NULL,
+    Agdm_HoraFim TIME NOT NULL,
+    Agdm_Data DATE NOT NULL,
+    Agdm_Status CHAR(1) NOT NULL -- P - PENDENTE, A - ACEITO, C - CANCELADO PELO USUARIO, R - RECUSADO PELO BARBEIRO
+);
+
+CREATE TABLE barbearia_barbeiros(
+    Barb_Codigo INT NOT NULL,
+    Usr_Codigo INT NOT NULL,
+    BarbB_Especialidade VARCHAR(255)
+);
+
+CREATE TABLE barbeiro_avaliacoes(
+    Usr_Codigo INT NOT NULL,
+    Aval_Comentario TEXT,
+    Aval_Rate INT NOT NULL
+);
+
+CREATE TABLE barbearia_avaliacoes(
+    Barb_Codigo INT NOT NULL,
+    Aval_Comentario TEXT,
+    Aval_Rate INT NOT NULL
+);
+
+ALTER TABLE barbearia_contatos ADD CONSTRAINT fk_barbearia_contatos_barbearia
 FOREIGN KEY(Barb_Codigo) REFERENCES barbearia(Barb_Codigo);
 
 ALTER TABLE barbearia_proprietarios ADD CONSTRAINT pk_barbearia_proprietario
 PRIMARY KEY(Barb_Codigo, Usr_Codigo);
 
-ALTER TABLE barbearia_proprietarios ADD CONSTRAINT fk_barbearia_proprietario_barbeiro
+ALTER TABLE barbearia_proprietarios ADD CONSTRAINT fk_barbearia_proprietario_barbearia
 FOREIGN KEY(Barb_Codigo) REFERENCES barbearia(Barb_Codigo);
 
 ALTER TABLE barbearia_proprietarios ADD CONSTRAINT fk_barbearia_proprietario_usuario
@@ -68,3 +116,36 @@ FOREIGN KEY(Barb_Codigo) REFERENCES barbearia(Barb_Codigo);
 
 ALTER TABLE servico ADD CONSTRAINT fk_servico_categoria
 FOREIGN KEY(ServCat_Codigo) REFERENCES servico_categorias(ServCat_Codigo);
+
+ALTER TABLE servico_imagens ADD CONSTRAINT fk_servico_imagens_servico
+FOREIGN KEY(Serv_Codigo) REFERENCES servico(Serv_Codigo);
+
+ALTER TABLE barbearia_horarios ADD CONSTRAINT fk_barbearia_horarios_barbearia
+FOREIGN KEY(Barb_Codigo) REFERENCES barbearia(Barb_Codigo);
+
+ALTER TABLE agendamento ADD CONSTRAINT fk_agendamento_barbearia
+FOREIGN KEY(Barb_Codigo) REFERENCES barbearia(Barb_Codigo);
+
+ALTER TABLE agendamento ADD CONSTRAINT fk_agendamento_barbeiro
+FOREIGN KEY(Agdm_Barbeiro) REFERENCES usuario(Usr_Codigo);
+
+ALTER TABLE agendamento ADD CONSTRAINT fk_agendamento_usuario
+FOREIGN KEY(Usr_Codigo) REFERENCES usuario(Usr_Codigo);
+
+ALTER TABLE agendamento ADD CONSTRAINT fk_agendamento_servico
+FOREIGN KEY(Serv_Codigo) REFERENCES servico(Serv_Codigo);
+
+ALTER TABLE barbearia_barbeiros ADD CONSTRAINT pk_barbearia_barbeiro
+PRIMARY KEY(Barb_Codigo, Usr_Codigo);
+
+ALTER TABLE barbearia_barbeiros ADD CONSTRAINT fk_barbearia_barbeiros_barbearia
+FOREIGN KEY(Barb_Codigo) REFERENCES barbearia(Barb_Codigo);
+
+ALTER TABLE barbearia_barbeiros ADD CONSTRAINT fk_barbearia_barbeiros_usuario
+FOREIGN KEY(Usr_Codigo) REFERENCES usuario(Usr_Codigo);
+
+ALTER TABLE barbeiro_avaliacoes ADD CONSTRAINT fk_barbeiro_avaliacoes_usuario
+FOREIGN KEY(Usr_Codigo) REFERENCES usuario(Usr_Codigo);
+
+ALTER TABLE barbearia_avaliacoes ADD CONSTRAINT fk_barbearia_avaliacoes_barbearia
+FOREIGN KEY(Barb_Codigo) REFERENCES barbearia(Barb_Codigo);
---
+++ sql/selects.sql
@@ -0,0 +1,28 @@
+-- Horarios disponiveis sem considerar agendamento, somente o dia
+SELECT H.* FROM horarios H, barbearia_horarios BH
+WHERE H.Horario >= BH.BarbH_HoraInicio
+AND H.Horario <= BH.BarbH_HoraFim
+AND BH.BarbH_Dia = "TER";
+
+-- Horarios ja agendados por codigo do barbeiro e data de agendamento
+SELECT H.* FROM horarios H, agendamento A
+WHERE H.Horario >= A.Agdm_HoraInicio
+AND H.Horario <= A.Agdm_HoraFim
+AND A.Agdm_Barbeiro = 1
+AND A.Agdm_Data = '2023-04-04'
+AND A.Agdm_Status NOT IN ('C', 'R');
+
+-- Traz somente horarios disponiveis para agendamento
+SELECT * FROM horarios
+WHERE Horario NOT IN (
+SELECT H.Horario FROM horarios H, agendamento A
+WHERE H.Horario >= A.Agdm_HoraInicio
+AND H.Horario <= A.Agdm_HoraFim
+AND A.Agdm_Barbeiro = 1
+AND A.Agdm_Data = '2023-04-04'
+AND A.Agdm_Status NOT IN ('C', 'R'))
+AND Horario IN (
+SELECT H.Horario FROM horarios H, barbearia_horarios BH
+WHERE H.Horario >= BH.BarbH_HoraInicio
+AND H.Horario <= BH.BarbH_HoraFim
+AND BH.BarbH_Dia = "TER");
