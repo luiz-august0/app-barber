797e41e3f11a08b4f8831478995dbb46ebf881f4
luiz-august0 <66564172+luiz-august0@users.noreply.github.com>
2023-04-18 21:44:32 -0300

    Implantando endpoints para serviços e categorias

--- api/routes.js
+++ api/routes.js
@@ -1,17 +1,14 @@
 import { Router, static as Static } from "express";
-import multer from "multer";
 import SessionController from "./routes/SessionController";
 import auth from "./middlewares/auth";
 import UsuarioController from "./routes/UsuarioController";
 import BarbeariaController from "./routes/BarbeariaController";
-import { storage } from './uploadFile';
+import BarbeariaHorariosController from "./routes/BarbeariaHorariosController";
+import BarbeariaServicosController from "./routes/BarbeariaServicosController";
 const uploadFile = require('./uploadFile');
-
-const upload = multer({ storage: storage });
 
 const routes = new Router();
 
-routes.use('/upload_files', Static("upload_files"));
 routes.post('/usuario', UsuarioController.create);
 routes.post('/usuarioVerify', UsuarioController.verify);
 routes.post('/usuario_emailrecuperacao', UsuarioController.postEnviaEmailRecuperacaoSenha);
@@ -64,10 +61,34 @@ routes.post('/barbearia_logo/:id', (req, res) => {
 		})
 		.catch((err) => res.status(500).json(err));
 });
-routes.get('/barbearia_horarios', BarbeariaController.getHorarios);
-routes.post('/barbearia_horarios_dia/:id', BarbeariaController.getBarbeariaHorariosDia);
-routes.post('/barbearia_horariodia_post/:id', BarbeariaController.postBarbeariaHorarioDia);
-routes.post('/barbearia_horariodia_update/:id', BarbeariaController.updateBarbeariaHorarioDia);
-routes.delete('/barbearia_horariodia_delete/:id', BarbeariaController.deleteBarbeariaHorarioDia);
+routes.get('/barbearia_horarios', BarbeariaHorariosController.getHorarios);
+routes.post('/barbearia_horarios_dia/:id', BarbeariaHorariosController.getBarbeariaHorariosDia);
+routes.post('/barbearia_horariodia_post/:id', BarbeariaHorariosController.postBarbeariaHorarioDia);
+routes.post('/barbearia_horariodia_update/:id', BarbeariaHorariosController.updateBarbeariaHorarioDia);
+routes.delete('/barbearia_horariodia_delete/:id', BarbeariaHorariosController.deleteBarbeariaHorarioDia);
+routes.get('/barbearia_categoria/:id', BarbeariaServicosController.getBarbeariaCategorias);
+routes.get('/barbearia_categoriaById/:id', BarbeariaServicosController.showBarbeariaCategoria);
+routes.post('/barbearia_categoria', BarbeariaServicosController.postBarbeariaCategoria);
+routes.put('/barbearia_categoria/:id', BarbeariaServicosController.updateBarbeariaCategoria);
+routes.delete('/barbearia_categoria/:id', BarbeariaServicosController.deleteBarbeariaCategoria);
+routes.get('/barbearia_servicoscategoria/:id', BarbeariaServicosController.getBarbeariaCategoriaServicos);
+routes.get('/barbearia_servico/:id', BarbeariaServicosController.showBarbeariaServico);
+routes.post('/barbearia_servico', BarbeariaServicosController.postBarbeariaServico);
+routes.put('/barbearia_servico/:id', BarbeariaServicosController.updateBarbeariaServico);
+routes.delete('/barbearia_servico/:id', BarbeariaServicosController.deleteBarbeariaServico);
+routes.get('/barbearia_servicoimagens/:id', BarbeariaServicosController.getImagensServico);
+routes.post('/barbearia_servicoimagem/:id', (req, res) => {
+	uploadFile(req.body.file)
+		.then((url) => {
+			try {
+				BarbeariaServicosController.postImagemServico(req.params.id, url);
+				return res.status(201).json(url);
+			} catch (err) {
+				res.status(500).json(err);
+			}
+		})
+		.catch((err) => res.status(500).json(err));
+});
+routes.post('/barbearia_servicoimagem_remove/:id', BarbeariaServicosController.deleteImagemServico);
 
 export default routes;
